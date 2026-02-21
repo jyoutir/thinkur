e5bc322bb3c849004526aa73baca83dcbd4e08fa
jyoutir <jyoutirraj2004@gmail.com>
2026-02-21 14:11:33 +0000

    UX - Extend when click idle mode

--- Sources/thinkur/UI/MainWindow/MainWindowView.swift
+++ Sources/thinkur/UI/MainWindow/MainWindowView.swift
@@ -2,17 +2,13 @@ import SwiftUI
 
 struct MainWindowView: View {
     @State private var selectedPage: NavigationPage = .home
-    @Environment(\.colorScheme) private var colorScheme
 
     var body: some View {
-        GlassEffectContainer {
-            NavigationSplitView {
-                SidebarView(selectedPage: $selectedPage)
-            } detail: {
-                ContentRouter(page: selectedPage)
-            }
+        NavigationSplitView {
+            SidebarView(selectedPage: $selectedPage)
+        } detail: {
+            ContentRouter(page: selectedPage)
         }
-        .id(colorScheme)
         .frame(minWidth: 920, minHeight: 620)
     }
 }
