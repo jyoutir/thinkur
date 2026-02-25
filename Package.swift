865c3bf07439bce3dadd933459539cdf570d7bce
jyoutir <jyoutirraj2004@gmail.com>
2026-02-25 22:25:54 +0000

    added setting for show in dock, added meny bar, fixed window close behaviour

--- Package.swift
+++ Package.swift
@@ -9,12 +9,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/argmaxinc/WhisperKit.git", from: "0.9.0"),
+        .package(url: "https://github.com/TelemetryDeck/SwiftSDK.git", from: "2.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "thinkur",
             dependencies: [
                 .product(name: "WhisperKit", package: "WhisperKit"),
+                .product(name: "TelemetryDeck", package: "SwiftSDK"),
             ],
             path: "Sources/thinkur",
             swiftSettings: [
